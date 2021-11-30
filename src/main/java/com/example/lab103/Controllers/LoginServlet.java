c9fd07f04ded6dab4b7f104b68adbc4bee0be307
Percy De La Rosa <a20192265@pucp.edu.pe>
2021-11-29 20:22:13 -0500

    Version 2, ahora si corre

---
+++ src/main/java/com/example/lab103/Controllers/LoginServlet.java
@@ -0,0 +1,71 @@
+package com.example.lab103.Controllers;
+
+import com.example.lab103.Beans.Employee;
+import com.example.lab103.Daos.EmployeeDao;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+import java.io.IOException;
+
+@WebServlet(name = "LoginServlet", urlPatterns = {"/LoginServlet"})
+public class LoginServlet extends HttpServlet {
+
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        request.setCharacterEncoding("UTF-8");
+        String action = request.getParameter("action") == null ? "loginform" : request.getParameter("action");
+        HttpSession session = request.getSession();
+
+        switch (action) {
+            case "loginform":
+                Employee employeeSession = (Employee) session.getAttribute("employeeSession");
+                if (employeeSession != null && employeeSession.getEmployeeId() > 0) {
+                    response.sendRedirect(request.getContextPath() + "/EmployeeServlet");
+                } else {
+                    RequestDispatcher view = request.getRequestDispatcher("login/loginForm.jsp");
+                    view.forward(request, response);
+                }
+                break;
+            case "logout":
+                session.invalidate();
+                response.sendRedirect(request.getContextPath());
+                break;
+        }
+    }
+
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+
+
+        String username = request.getParameter("inputEmail");
+        String password = request.getParameter("inputPassword");
+
+        if (username == null || password == null) {
+            request.setAttribute("err", "El usuario o password no pueden ser vacíos");
+            RequestDispatcher view = request.getRequestDispatcher("login/loginForm.jsp");
+            view.forward(request, response);
+        }else{
+            EmployeeDao employeeDao = new EmployeeDao();
+            Employee employee = employeeDao.validarUsuarioPasswordHashed(username, password);
+
+            if (employee != null) {
+                HttpSession session = request.getSession();
+                session.setAttribute("employeeSession", employee);
+
+                session.setMaxInactiveInterval(10 * 60); // 10 minutos
+
+                response.sendRedirect(request.getContextPath() + "/EmployeeServlet");
+            } else {
+                request.setAttribute("err", "El usuario o password no existen");
+                RequestDispatcher view = request.getRequestDispatcher("login/loginForm.jsp");
+                view.forward(request, response);
+            }
+        }
+
+    }
+}
---
+++ src/main/java/com/example/lab103/Dtos/EmpleadosPorRegionDto.java
@@ -0,0 +1,23 @@
+package com.example.lab103.Dtos;
+
+public class EmpleadosPorRegionDto {
+
+    private String nombreRegion;
+    private int cantidadEmpleados;
+
+    public String getNombreRegion() {
+        return nombreRegion;
+    }
+
+    public void setNombreRegion(String nombreRegion) {
+        this.nombreRegion = nombreRegion;
+    }
+
+    public int getCantidadEmpleados() {
+        return cantidadEmpleados;
+    }
+
+    public void setCantidadEmpleados(int cantidadEmpleados) {
+        this.cantidadEmpleados = cantidadEmpleados;
+    }
+}
